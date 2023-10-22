a38964e57ae54cbafe318ce08bcb840f370dec54
Kayky de Brito <oss@kaykyb.com>
2023-10-21 21:08:38 -0300

    Init project

---
+++ lib/ex_archestria.ex
@@ -0,0 +1,18 @@
+defmodule ExArchestria do
+  @moduledoc """
+  Documentation for `ExArchestria`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExArchestria.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_archestria_test.exs
@@ -0,0 +1,8 @@
+defmodule ExArchestriaTest do
+  use ExUnit.Case
+  doctest ExArchestria
+
+  test "greets the world" do
+    assert ExArchestria.hello() == :world
+  end
+end
